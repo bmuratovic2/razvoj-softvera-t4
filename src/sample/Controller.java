4b698f064b700c5b55043d5a104971313c97a34c
bmuratovic2 <ungohlohzoir1>
2019-04-03 10:21:38 +0200

    innitial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+
+public class Controller{
+    public TextArea rezulat;
+    public TextArea unos;
+    private ArrayList<Artikal> artikli = new ArrayList<>();
+    public void Dodaj(ActionEvent actionEvent){
+        String[] artikliStr = unos.getText().split("\n");
+        for(String linija : artikliStr)
+            artikli.add(new Artikal(linija));
+
+        Artikal.izbaciDuplikate(artikli);
+
+        String res= "";
+        for(Artikal artikal : artikli){
+            res=Artikal + "\n";
+        }
+        rezultat.setText(res);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
